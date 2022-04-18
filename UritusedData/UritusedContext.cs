c5d5f09ed2278ba9865b9dcf1d3623b5e3561ce3
kukeke11 <martinring@hotmail.com>
2022-04-18 11:41:31 +0300

    f

--- UritusedData/UritusedContext.cs
+++ UritusedData/UritusedContext.cs
@@ -6,8 +6,11 @@ namespace UritusedData
     //sessiooni andmebaas mille abil saab teha CRUD operatsioone
     public class UritusedContext : DbContext
     {
+        //Andmebaas
         public UritusedContext(DbContextOptions options) : base(options) { }
 
+
+        //Andmebaasi tabelid
         public DbSet<Osalejad> Osalejad { get; set; }
         public DbSet<Uritused> Uritused { get; set; }
         public DbSet<Ettevote> Ettevotted { get; set; }
